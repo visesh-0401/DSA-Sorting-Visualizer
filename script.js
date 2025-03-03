c03fba832e6960cc27bb98ec20da8202e10de42b
visesh-0401 <viseshchauhan4104@gmail.com>
2025-03-03 21:33:14 +0530

    Added Selection Sort & Insertion Sort

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function generateArray() {
 }
 
 // Sorting Algorithms
+
+// bubble sort
 async function bubbleSort() {
     let bars = document.getElementsByClassName("bar");
     let len = bars.length;
@@ -34,6 +36,63 @@ async function bubbleSort() {
             bars[j].style.backgroundColor = "blue";
             bars[j + 1].style.backgroundColor = "blue";
         }
+    }
+}
+
+// Selection Sort
+async function selectionSort() {
+    let bars = document.getElementsByClassName("bar");
+    let len = bars.length;
+
+    for (let i = 0; i < len - 1; i++) {
+        let minIndex = i;
+        bars[minIndex].style.backgroundColor = "red"; // Highlight the minimum bar
+
+        for (let j = i + 1; j < len; j++) {
+            bars[j].style.backgroundColor = "yellow"; // Highlight current comparison
+            await new Promise((resolve) => setTimeout(resolve, 100));
+
+            if (parseInt(bars[j].style.height) < parseInt(bars[minIndex].style.height)) {
+                bars[minIndex].style.backgroundColor = "blue"; // Reset previous min
+                minIndex = j;
+                bars[minIndex].style.backgroundColor = "red"; // Highlight new min
+            } else {
+                bars[j].style.backgroundColor = "blue"; // Reset non-min bars
+            }
+        }
+
+        if (minIndex !== i) {
+            let tempHeight = bars[i].style.height;
+            bars[i].style.height = bars[minIndex].style.height;
+            bars[minIndex].style.height = tempHeight;
+        }
+
+        bars[minIndex].style.backgroundColor = "blue";
+        bars[i].style.backgroundColor = "green"; // Mark sorted element
+    }
+}
+
+// insertion Sort
+async function insertionSort() {
+    let bars = document.getElementsByClassName("bar");
+    let len = bars.length;
+
+    for (let i = 1; i < len; i++) {
+        let key = parseInt(bars[i].style.height);
+        let j = i - 1;
+        bars[i].style.backgroundColor = "red"; // Highlight the key element
+
+        await new Promise((resolve) => setTimeout(resolve, 200));
+
+        while (j >= 0 && parseInt(bars[j].style.height) > key) {
+            bars[j + 1].style.height = bars[j].style.height;
+            bars[j].style.backgroundColor = "yellow"; // Highlight moving elements
+            await new Promise((resolve) => setTimeout(resolve, 100));
+            j--;
+        }
+        bars[j + 1].style.height = `${key}px`;
+
+        bars[i].style.backgroundColor = "green"; // Mark sorted element
     }
 }
 
